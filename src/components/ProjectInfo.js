089f127f74da6c048605f14b2e1a159369983307
DueMinh <thi.thuy.hoang.0907@gmail.com>
2022-05-28 11:36:33 +0700

    update components

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -8,7 +8,13 @@ export default class ProjectInfo extends Component {
       <div className ="project">
           <h2>Project</h2>
           
-          <p> The project is to create </p>
+       
+          <h3> Our ambition</h3>
+          <p> Our project includes three ambitious peoples: two back-end developers and one front-end.
+            With our ambition, We want to build a place for all bikers in the world to follow their 
+            ambitions and create a community for them. To do so, we are building an NFT collections and afterward 
+            a market place where bikers can mint, trade their NFT collections. After,we can operate the competititions 
+            between bikers all over the world. </p>
           
           </div>
     )
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import './Team.css'; 
 
 export default class Team extends Component {
   render() {
     return (
       <div className ="team">
-          Team
+          <h2 className = "h2Aboutus"> About us</h2>
+         
+          <p className ="pAboutus"> Our project includes three ambitious peoples: two back-end developers and one front-end.
+            
+     </p>
           </div>
     )
   }
